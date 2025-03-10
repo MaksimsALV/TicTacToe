ee492b50a5522d0daf216e2ad89186d4279616a0
MaksimsALV <maxim.ahmetov@gmail.com>
2025-03-10 23:34:31 +0200

    added Stats logic to return playerStats

--- src/game/tictactoe/menu/Stats.java
+++ src/game/tictactoe/menu/Stats.java
@@ -1,16 +1,18 @@
 package game.tictactoe.menu;
 
+import game.tictactoe.repository.PlayerStats;
+
 import java.util.Scanner;
 
 public class Stats {
     public static void viewStats() {
         Scanner s = new Scanner(System.in);
         System.out.println("These are the stats for current game session:");
-        System.out.println("Player 1 won:" ); //todo add how many games player 1 won from PlayerStats
-        System.out.println("Player 2 won:" ); //todo add how many games player 2 won from PlayerStats
-        System.out.print("=================================");
+        PlayerStats.playerStats.forEach((playerName, wins) ->
+                System.out.println(playerName + " won: " + wins));
+        System.out.println("=================================");
         while (true) {
-            System.out.print("Type 0 to return back to main menu");
+            System.out.println("Type 0 to return back to main menu");
             System.out.print("Input:");
             int choice = Integer.parseInt(s.nextLine());
             if (choice == 0) {
