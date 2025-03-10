883dc2b8c6cc26803b4d68cc3402e8a3b0d3abc9
MaksimsALV <maxim.ahmetov@gmail.com>
2025-03-10 22:32:55 +0200

    added Players and PlayerStats

---
+++ src/game/tictactoe/repository/PlayerStats.java
@@ -0,0 +1,4 @@
+package game.tictactoe.repository;
+
+public class PlayerStats {
+}
---
+++ src/game/tictactoe/repository/Players.java
@@ -0,0 +1,7 @@
+package game.tictactoe.repository;
+
+import java.util.HashMap;
+
+public class Players {
+    public static HashMap<Integer, String> playersList = new HashMap<>();
+}
