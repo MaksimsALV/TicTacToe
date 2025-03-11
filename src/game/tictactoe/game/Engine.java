47f70127947061791a24b5f82c51b5c79b309985
MaksimsALV <maxim.ahmetov@gmail.com>
2025-03-11 14:54:35 +0200

    adding updateBoard method

--- src/game/tictactoe/game/Engine.java
+++ src/game/tictactoe/game/Engine.java
@@ -17,8 +17,8 @@ public class Engine {
         System.out.println("Game Rules:");
         System.out.println("First who get 3 same symbols in a row wins!");
         System.out.println("==============================================");
-        System.out.println(PlayerStorage.playersList.get(0).playerName + " plays " + PlayerStorage.playersList.getFirst().playerSymbol);
-        System.out.println(PlayerStorage.playersList.get(1).playerName + " plays " + PlayerStorage.playersList.getLast().playerSymbol);
+        System.out.println(PlayerStorage.playersList.get(0).playerName + " plays " + PlayerStorage.playersList.get(0).playerSymbol);
+        System.out.println(PlayerStorage.playersList.get(1).playerName + " plays " + PlayerStorage.playersList.get(1).playerSymbol);
         System.out.println("==============================================");
 
         boolean playerOneTurn = true;
@@ -33,7 +33,8 @@ public class Engine {
             System.out.print("Input:");
             int choice = Integer.parseInt(ge.nextLine());
 
-            //todo update board
+            char symbol = playerOneTurn ? PlayerStorage.playersList.get(0).playerSymbol : PlayerStorage.playersList.get(1).playerSymbol;
+            updateBoard(choice, symbol);
             playerOneTurn = !playerOneTurn;
         }
     }
@@ -41,5 +42,16 @@ public class Engine {
         System.out.println("" + board[0][0] + " | " + board[0][1] + " | " + board[0][2]);
         System.out.println("" + board[1][0] + " | " + board[1][1] + " | " + board[1][2]);
         System.out.println("" + board[2][0] + " | " + board[2][1] + " | " + board[2][2]);
+    }
+    public static void updateBoard(int choice, char symbol) { //method to update matrix based on inputs
+        if (choice == 1) board[0][0] = symbol;
+        else if (choice == 2) board[0][1] = symbol;
+        else if (choice == 3) board[0][2] = symbol;
+        else if (choice == 4) board[1][0] = symbol;
+        else if (choice == 5) board[1][1] = symbol;
+        else if (choice == 6) board[1][2] = symbol;
+        else if (choice == 7) board[2][0] = symbol;
+        else if (choice == 8) board[2][1] = symbol;
+        else if (choice == 9) board[2][2] = symbol;
     }
 }
