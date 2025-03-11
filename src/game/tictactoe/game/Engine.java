7c8ac9d45415d242510876064acb4359707fc317
MaksimsALV <maxim.ahmetov@gmail.com>
2025-03-11 15:04:26 +0200

    refactoring Engine

--- src/game/tictactoe/game/Engine.java
+++ src/game/tictactoe/game/Engine.java
@@ -5,11 +5,6 @@ import game.tictactoe.repository.PlayerStorage;
 import java.util.Scanner;
 
 public class Engine {
-    public static char[][] board = {
-            {'1', '2', '3'},
-            {'4', '5', '6'},
-            {'7', '8', '9'}
-    };
 
     public static void gameEngine() {
         Scanner ge = new Scanner(System.in);
@@ -38,11 +33,22 @@ public class Engine {
             playerOneTurn = !playerOneTurn;
         }
     }
+
+    // this is our board
+    public static char[][] board = {
+            {'1', '2', '3'},
+            {'4', '5', '6'},
+            {'7', '8', '9'}
+    };
+
+    // this prints our board (matrix) according to our array from above
     public static void printBoard() { //matrix look, first number is row; second number is column
         System.out.println("" + board[0][0] + " | " + board[0][1] + " | " + board[0][2]);
         System.out.println("" + board[1][0] + " | " + board[1][1] + " | " + board[1][2]);
         System.out.println("" + board[2][0] + " | " + board[2][1] + " | " + board[2][2]);
     }
+
+    // this updates our board after each player input
     public static void updateBoard(int choice, char symbol) { //method to update matrix based on inputs
         if (choice == 1) board[0][0] = symbol;
         else if (choice == 2) board[0][1] = symbol;
