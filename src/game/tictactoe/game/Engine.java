6b4031b23c0ec67fcfe9e85ee0c48e29fbb3fdc8
MaksimsALV <maxim.ahmetov@gmail.com>
2025-03-11 17:04:52 +0200

    moving logic block into logic class

--- src/game/tictactoe/game/Engine.java
+++ src/game/tictactoe/game/Engine.java
@@ -1,5 +1,6 @@
 package game.tictactoe.game;
 
+import game.tictactoe.game.logic.Logic;
 import game.tictactoe.repository.PlayerStorage;
 
 import java.util.Scanner;
@@ -24,40 +25,13 @@ public class Engine {
             } else {
                 System.out.println("Player 2 turn");
             }
-            printBoard();
+            Logic.printBoard();
             System.out.print("Input:");
             int choice = Integer.parseInt(ge.nextLine());
 
             char symbol = playerOneTurn ? PlayerStorage.playersList.get(0).playerSymbol : PlayerStorage.playersList.get(1).playerSymbol;
-            updateBoard(choice, symbol);
+            Logic.updateBoard(choice, symbol);
             playerOneTurn = !playerOneTurn;
         }
-    }
-
-    // this is our board
-    public static char[][] board = {
-            {'1', '2', '3'},
-            {'4', '5', '6'},
-            {'7', '8', '9'}
-    };
-
-    // this prints our board (matrix) according to our array from above
-    public static void printBoard() { //matrix look, first number is row; second number is column
-        System.out.println("" + board[0][0] + " | " + board[0][1] + " | " + board[0][2]);
-        System.out.println("" + board[1][0] + " | " + board[1][1] + " | " + board[1][2]);
-        System.out.println("" + board[2][0] + " | " + board[2][1] + " | " + board[2][2]);
-    }
-
-    // this updates our board after each player input, if player puts input on any of the numbers, we replace that board locator with symbol
-    public static void updateBoard(int choice, char symbol) { //method to update matrix based on inputs
-        if (choice == 1) board[0][0] = symbol;
-        else if (choice == 2) board[0][1] = symbol;
-        else if (choice == 3) board[0][2] = symbol;
-        else if (choice == 4) board[1][0] = symbol;
-        else if (choice == 5) board[1][1] = symbol;
-        else if (choice == 6) board[1][2] = symbol;
-        else if (choice == 7) board[2][0] = symbol;
-        else if (choice == 8) board[2][1] = symbol;
-        else if (choice == 9) board[2][2] = symbol;
     }
 }
--- src/game/tictactoe/game/logic/Logic.java
+++ src/game/tictactoe/game/logic/Logic.java
@@ -3,7 +3,30 @@ package game.tictactoe.game.logic;
 import game.tictactoe.game.Engine;
 
 public class Logic {
-    public static void engineLogic() {
-        //todo firstly will try to do this logic inside Engine class then will transfer
+        // this is our board
+        public static char[][] board = {
+                {'1', '2', '3'},
+                {'4', '5', '6'},
+                {'7', '8', '9'}
+        };
+
+        // this prints our board (matrix) according to our array from above
+        public static void printBoard() { //matrix look, first number is row; second number is column
+            System.out.println("" + board[0][0] + " | " + board[0][1] + " | " + board[0][2]);
+            System.out.println("" + board[1][0] + " | " + board[1][1] + " | " + board[1][2]);
+            System.out.println("" + board[2][0] + " | " + board[2][1] + " | " + board[2][2]);
+        }
+
+        // this updates our board after each player input, if player puts input on any of the numbers, we replace that board locator with symbol
+        public static void updateBoard(int choice, char symbol) { //method to update matrix based on inputs
+            if (choice == 1) board[0][0] = symbol;
+            else if (choice == 2) board[0][1] = symbol;
+            else if (choice == 3) board[0][2] = symbol;
+            else if (choice == 4) board[1][0] = symbol;
+            else if (choice == 5) board[1][1] = symbol;
+            else if (choice == 6) board[1][2] = symbol;
+            else if (choice == 7) board[2][0] = symbol;
+            else if (choice == 8) board[2][1] = symbol;
+            else if (choice == 9) board[2][2] = symbol;
+        }
     }
-}
