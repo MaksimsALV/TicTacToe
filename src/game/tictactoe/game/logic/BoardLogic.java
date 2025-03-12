6edadffd2fc7c437c1795062b18dd5f63f07bb4c
MaksimsALV <maxim.ahmetov@gmail.com>
2025-03-12 22:12:34 +0200

    bigger board

--- src/game/tictactoe/game/logic/BoardLogic.java
+++ src/game/tictactoe/game/logic/BoardLogic.java
@@ -11,9 +11,13 @@ public class BoardLogic {
 
     // this prints our board (matrix) according to our array from above
     public static void printBoard() { //matrix look, first number is row; second number is column
-        System.out.println("" + board[0][0] + " | " + board[0][1] + " | " + board[0][2]);
-        System.out.println("" + board[1][0] + " | " + board[1][1] + " | " + board[1][2]);
-        System.out.println("" + board[2][0] + " | " + board[2][1] + " | " + board[2][2]);
+        System.out.println("                                                                ");
+        System.out.println("  " + board[0][0] + "  |  " + board[0][1] + "  |  " + board[0][2]);
+        System.out.println("-----|-----|-----");
+        System.out.println("  " + board[1][0] + "  |  " + board[1][1] + "  |  " + board[1][2]);
+        System.out.println("-----|-----|-----");
+        System.out.println("  " + board[2][0] + "  |  " + board[2][1] + "  |  " + board[2][2]);
+        System.out.println("                                                                ");
     }
 
     // this updates our board after each player input, if player puts input on any of the numbers, we replace that board locator with symbol
